293c71307342228983d22ac75c888603ab3e2d69
Andrejpadd <andrejpaduevic@gmail.com>
2026-07-29 08:59:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const btnTop = document.getElementById("scroll-to-top");
             let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
             let windowHeight = document.documentElement.scrollHeight - window.innerHeight;
         
-            // 1. Логіка для кнопки "Вгору" (показуємо, коли гортаємо вгору і ми не на самому верху)
+            
             if (scrollTop < lastScrollTop && scrollTop > 200) {
                 btnTop.classList.add("show");
             } else {
                 btnTop.classList.remove("show");
             }
         
-            // 2. Логіка для кнопки "Вниз" (показуємо, коли гортаємо вниз і ми далеко від низу)
+        
             if (scrollTop > lastScrollTop && scrollTop < windowHeight - 200) {
                 btnBottom.classList.add("show");
             } else {
@@ -24,7 +24,7 @@ const btnTop = document.getElementById("scroll-to-top");
             lastScrollTop = scrollTop;
         });
         
-        // Функція для скролу вгору
+        
         btnTop.addEventListener("click", function() {
             window.scrollTo({
                 top: 0,
@@ -32,7 +32,6 @@ const btnTop = document.getElementById("scroll-to-top");
             });
         });
         
-        // Функція для скролу вниз
         btnBottom.addEventListener("click", function() {
             window.scrollTo({
                 top: document.documentElement.scrollHeight,
